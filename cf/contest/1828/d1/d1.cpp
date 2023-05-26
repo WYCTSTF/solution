58d1f8f43cce6c88d4b19c1782dc6f0165b940ea
WYCTSTF <996098413@qq.com>
2023-05-26 16:39:07 +0800

    2023.5.26

--- cf/contest/1828/d1/d1.cpp
+++ cf/contest/1828/d1/d1.cpp
@@ -1,10 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
+using std::cin,std::cout,std::vector;
+using i64=long long;
+void solve(){
+  int n;cin>>n;
+  vector<int>a(n);
+  for(int &x:a)cin>>x;
+  i64 ans=0;
+  for(int i=0;i<n;++i){
+    vector<std::pair<int,int>>st;
+    for(int j=i;j<n;++j){
+      std::pair<int,int>p{a[j],a[j]};
+      while(!st.empty()&&st.back().second>a[j]){
+        p.second=std::max(p.second,st.back().second);
+        p.first=std::min(p.first,st.back().first);
+        st.pop_back();
+      }
+      st.push_back(p);
+      ans+=j-i+1-st.size();
+    }
+  }
+  cout<<ans<<'\n';
+}
 int main() {
   int tt;
-  cin >> tt;
-  while (tt--) {
-
+  cin>>tt;
+  while(tt--){
+    solve();
   }
   return 0;
 }
---
+++ cf/contest/1832/a/a.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include <dbg.h>
+#else
+#define dbg(...) 42
+#endif
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned int;
+using db = double;
+
+using std::cin, std::cout;
+
+#define rep(i,a,n) for(int i=a;i<=n;i++)
+#define per(i,a,n) for(int i=n;i>=a;i--)
+#define pb push_back
+#define fi first
+#define se second
+#define pi acos(-1)
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+typedef std::vector<int> VI;
+typedef std::pair<int,int> PII;
+
+i64 gcd(i64 a,i64 b) { return b?gcd(b,a%b):a; }
+
+void solve(){
+  std::string s;
+  cin>>s;
+  char c=s[0];
+  bool flag=true;
+  for(int i=0;i<sz(s)/2;++i)
+    if(s[i]!=c)flag=false;
+  puts(flag?"NO":"YES");
+}
+
+int main() {
+  int tt;cin>>tt;
+  while(tt--){
+    solve();
+  }
+  return 0;
+}
