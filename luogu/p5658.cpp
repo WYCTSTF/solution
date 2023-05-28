3a8518e687ca7d1691eececb90bba98a3a08980d
WYCTSTF <996098413@qq.com>
2023-05-29 00:44:16 +0800

    2023.5.29

---
+++ luogu/p5658.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  vector<int> fa(n + 1);
+  string s;
+  cin >> s;
+  for (int i = 2; i <= n; ++i)
+    cin >> fa[i];
+  
+  return 0;
+}
--- template/vector.cpp
+++ template/vector.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#include<dbg.h>
+
 vector <int> a, b, c;
 
 void init() {
@@ -12,38 +14,40 @@ void init() {
 }
 
 int main() {
-    vector <int> tmp = {1, 2, 3, 4, 5}; //c++98应该不支持{...}声明
+    vector <int> tmp {1, 2, 3, 4, 5}; //c++98应该不支持{...}声明
     for (int i = 0; i < 10; i++) {
         tmp.push_back(i);
     }
-    
-    // vector可以用以下这三种方式遍历
-    vector <int>::iterator it;
-    for (it = tmp.begin(); it != tmp.end(); it++) {
-        cerr << *it << ' ';
-    }
-    cerr << '\n';
-    
-    for (int i = 0; i < tmp.size(); i++) { // 使用小于号的原因：c++大部分容器都是左闭右开的
-        cerr << a[i] << ' ';
-    }
-    cerr << '\n';
-    
-    for (auto it : tmp) {
-        cerr << tmp[it] << ' ';
-    }
-    cerr << '\n';
-    
-    vector <int> arr(10, -1); // initialize an array of 10 -1
-    arr.resize(5, 0); // resize the vector
-    cout << arr.size() << ' ' << typeid(arr.size()).name() << endl;
-    for (int i = 0; i < (int)arr.size(); i++) {
-        cout << arr[i] << ' ';
-    }
 
-    /* arr.erase(arr.begin() + 3, arr.end()); delete all the element after the third element */
-    sort (arr.begin(), arr.end());
-    cerr << (int)(unique(arr.begin(), arr.end())-arr.begin()) << '\n';
-    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    dbg(tmp);
+    
+//     // vector可以用以下这三种方式遍历
+//     vector <int>::iterator it;
+//     for (it = tmp.begin(); it != tmp.end(); it++) {
+//         cerr << *it << ' ';
+//     }
+//     cerr << '\n';
+//     
+//     for (int i = 0; i < tmp.size(); i++) { // 使用小于号的原因：c++大部分容器都是左闭右开的
+//         cerr << a[i] << ' ';
+//     }
+//     cerr << '\n';
+//     
+//     for (auto it : tmp) {
+//         cerr << tmp[it] << ' ';
+//     }
+//     cerr << '\n';
+//     
+//     vector <int> arr(10, -1); // initialize an array of 10 -1
+//     arr.resize(5, 0); // resize the vector
+//     cout << arr.size() << ' ' << typeid(arr.size()).name() << endl;
+//     for (int i = 0; i < (int)arr.size(); i++) {
+//         cout << arr[i] << ' ';
+//     }
+// 
+//     /* arr.erase(arr.begin() + 3, arr.end()); delete all the element after the third element */
+//     sort (arr.begin(), arr.end());
+//     cerr << (int)(unique(arr.begin(), arr.end())-arr.begin()) << '\n';
+//     arr.erase(unique(arr.begin(), arr.end()), arr.end());
     return 0;
 }
