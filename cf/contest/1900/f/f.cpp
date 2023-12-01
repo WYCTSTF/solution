3b176695c2b5cab6ed16f1b9dd60d0c9e5d30e51
WYCTSTF <996098413@qq.com>
2023-12-01 17:04:57 +0800

    20231201

---
+++ cf/contest/1900/f/f.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned int;
+using db = double;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+
+#define rep(i,a,n) for(int i=a;i<=n;i++)
+#define per(i,a,n) for(int i=n;i>=a;i--)
+#define pb push_back
+#define fi first
+#define se second
+#define pi acos(-1)
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+
+i64 gcd(i64 a,i64 b) { return b?gcd(b,a%b):a; }
