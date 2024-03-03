0ced457c3a29c663f3329b412f0f7795e5b6da91
WYCTSTF <996098413@qq.com>
2024-03-03 23:10:18 +0800

    20240303

--- sol.cpp
+++ sol.cpp
@@ -1,14 +1,6 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 int main() {
-  int var = 42;
-  int& l_val = var;
-  int &&r_val = var + 40;
-  std::cout << l_val << '\n';
-  std::cout << r_val << '\n';
-  var = 45;
-  std::cout << l_val << '\n';
-  std::cout << r_val << '\n';
+  int n, m;
+  std::cin >> n >> m;
   return 0;
 }
