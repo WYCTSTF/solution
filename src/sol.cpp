19c58cbf1d8389640f44b407fdc34832fcd367a8
WYCTSTF <996098413@qq.com>
2025-05-19 14:18:46 +0800

    2025.5.19

--- src/sol.cpp
+++ src/sol.cpp
@@ -1,43 +1,8 @@
 #include <iostream>
-#include <string>
-#include <set>
-#ifdef LOCAL
-#include "dbg.h"
-#else
-#define dbg(...) 42
-#endif
+
 int main() {
-  int tt;
-  std::cin >> tt;
-  while (tt--) {
-    std::string s;
-    std::cin >> s;
-    int sum = 0;
-    for (char c : s)
-      sum += c - '0';
-    dbg(sum);
-    std::set<int> v;
-    for (int i = 2; i * i <= sum; ++i)
-      if (sum % i == 0)
-        v.insert(i), v.insert(sum / i);
-    auto check = [&](int k) -> bool {
-      int sum = 0;
-      for (char c : s) { 
-        if (sum > k) return false;
-        if (sum == k) sum = 0;
-        sum += c - '0';
-      }
-      if (sum != 0 && sum != k)
-        return false;
-      return true;
-    };
-    bool fl = false;
-    for (auto i : v) {
-      if (check(i)) {
-        fl = true;
-        break;
-      }
-    }
-    std::cout << (fl ? "YES\n" : "NO\n");
-  }
+  int n, m;
+  std::cin >> n >> m;
+  std::cout << n << " " << m << std::endl;
+  return 0;
 }
