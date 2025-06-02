56d46fc3a2d624df5c1eb089913d1e75cfd0a23a
WYCTSTF <996098413@qq.com>
2025-06-02 15:08:48 +0800

    修改cmakelists，设置仅供windows使用的路径

--- src/sol.cpp
+++ src/sol.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 int main() {
   int n, m;
   std::cin >> n >> m;
-  std::cout << n << " " << m << std::endl;
+  std::cout << n + m << '\n';
   return 0;
 }
